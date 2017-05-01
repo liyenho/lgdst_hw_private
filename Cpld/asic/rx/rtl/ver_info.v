c1f6ee6430eccb65aa3f869e4352c4380d702eb5
liyen ho <lho@yuneecusa.com>
2017-05-01 10:19:44 -0700

    updated for ts_fail signal passthru

--- Cpld/asic/rx/rtl/ver_info.v
+++ Cpld/asic/rx/rtl/ver_info.v
@@ -3,8 +3,8 @@
 `define VER_CONFIG 8'h0
 
 `define BUILD_YEAR 8'h17
-`define BUILD_MONTH 8'h02
-`define BUILD_DAY 8'h14
-`define BUILD_HOUR 8'h14
-`define BUILD_MIN 8'h42
+`define BUILD_MONTH 8'h05
+`define BUILD_DAY 8'h01
+`define BUILD_HOUR 8'h10
+`define BUILD_MIN 8'h12
 
